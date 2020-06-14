747bfdb64fd1dd163b707607b919ab56f5911b8a
waleedalbahar <waleedalbahar10@gmail.com>
2020-06-14 17:02:57 +0300

    waleed

---
+++ وليد أنس البحر -662/MyPlayground.playground/Contents.swift
@@ -0,0 +1,33 @@
+
+let name = "Waleed AL Bahar"
+var age = 15
+var hobby1 = "football"
+var hobby2 = "swimming"
+var height = "170"
+print("my name is \(name), and my age is \(age) ,and my height is \(height), and (my hobbies) are: \(hobby1), \(hobby2)")
+let friendname = "Mohammed AL Mishri"
+var friendage = 15
+var friendhobby1 = "football"
+var friendhobby2 = "horseriding"
+var friendheight = "175"
+var age_difference = friendage - age
+age == friendage; "!false"
+age < friendage; "!true"
+age > friendage; "!true"
+age >= friendage;"!true"
+age <= friendage;"!true"
+age != friendage;"!true"
+name == friendname;"!true"
+name != friendname; "!false"
+hobby1 == friendhobby1; "!false"
+hobby1 != friendhobby1; "!true"
+hobby1 == friendhobby2; "!true"
+hobby1 != friendhobby2; "!false"
+hobby2 == friendhobby2; "!true"
+hobby2 != friendhobby2; "!false"
+height == friendheight; "!true"
+height != friendheight; "!false"
+height >= friendheight; "!true"
+height <= friendheight; "!false"
+height < friendheight; "!false"
+height > friendheight; "!false"
